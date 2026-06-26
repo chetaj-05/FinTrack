5617836afafc7de6d0896e43b2f86de0dd92e6a7
Chetaj Mahajan <your_github_email>
2026-06-27 01:28:06 +0530

    Configure MongoDB Atlas connection

--- server/server.js
+++ server/server.js
@@ -2,8 +2,23 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 5000;
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const dns=require("dns");
+dns.setServers([
+    '1.1.1.1',
+    '8.8.8.8'
+]
+)
 
+dotenv.config();
+console.log(process.env.MONGO_URI);
+
+connectDB();
+
+
+
+const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     res.send("Welcome to FinTrack API 🚀");
 });
